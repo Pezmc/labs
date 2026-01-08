e274e5bd19ec63ce185620f9cc2a9de9a7513173
Pez Cuckow <email@pezcuckow.com>
2026-01-08 10:59:34 +0100

    💄 Add Tailwind CSS theme with Hugo built-in support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './site/layouts/**/*.html',
+    './site/content/**/*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
